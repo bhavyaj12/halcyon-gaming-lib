4d0a0b59588d716186ac2fdd3284d0645207175b
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-05-12 09:49:26 +0530

    feat: added theme Provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import "./App.css";
+import { NavigationTop } from "components";
+import { useTheme } from "contexts/theme-context";
+import "styles/main.css";
+import "App.css";
 
 function App() {
+  const { theme } = useTheme();
   return (
-    <div className="App">
-      
+    <div className={`App ${theme}`}>
+      <NavigationTop />
+      <div className="page-container">
+        <h1>Hello</h1>
+      </div>
     </div>
   );
 }
