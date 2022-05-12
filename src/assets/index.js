120e48bb9b4b70df1a233501b74197f85e9fdfb7
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-05-13 03:47:43 +0530

    feat: added images for categories

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,10 @@
 import darkLogo from "assets/logos/dark-theme-vid-logo.svg";
 import lightLogo from "assets/logos/light-theme-vid-logo.svg";
 
-export { darkLogo, lightLogo };
+import AdventureCatg from "assets/images/game-catg-1.jpg";
+import HorrorCatg from "assets/images/game-catg-2.jpg"
+import ActionCatg from "assets/images/game-catg-3.jpg"
+import SimulationCatg from "assets/images/game-catg-4.jpg"
+import WalkthroughCatg from "assets/images/game-catg-5.jpg"
+
+export { darkLogo, lightLogo, AdventureCatg, HorrorCatg, ActionCatg, SimulationCatg, WalkthroughCatg };
