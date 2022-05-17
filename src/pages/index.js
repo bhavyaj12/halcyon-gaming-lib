e151cb37d672a31441b75db6788589638d1d6140
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-05-18 03:39:23 +0530

    feat: login and signup features with toast

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import HomePage from "./HomePage/HomePage";
 import InvalidPage from "./InvalidPage/InvalidPage";
+import LoginPage from "./AuthenticationPage/LoginPage";
+import SignupPage from "./AuthenticationPage/SignupPage";
+import HistoryPage from "./HistoryPage/HistoryPage";
 
-export { HomePage, InvalidPage };
+export { HomePage, InvalidPage, LoginPage, SignupPage, HistoryPage };
