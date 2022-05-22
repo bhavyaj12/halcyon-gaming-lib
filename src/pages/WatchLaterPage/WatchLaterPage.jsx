3bbaffd47449105366e941af5b57a78d1e9091bc
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-05-22 17:02:42 +0530

    feat: added watch later page

---
+++ src/pages/WatchLaterPage/WatchLaterPage.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import { NavigationSide } from "components";
+import { VideoCard } from "components";
+import { useUserData } from "contexts";
+
+const WatchLaterPage = () => {
+  const {
+    userDataState: { watchlater },
+  } = useUserData();
+
+  return (
+    <section className="video-main-content">
+      <div className="video-grid-item">
+        <NavigationSide />
+      </div>
+      <main className="video-grid-item" id="video-main">
+        <section className="video-container">
+          <div className="flex">
+            {watchlater.length > 0 ? (
+              watchlater.map((video) => {
+                return <VideoCard video={video} key={video._id} />;
+              })
+            ) : (
+              <>
+                <div className="alert alert-container alert-error">
+                  You do not have any videos in watch later. Add to watch later
+                  from the explore page.
+                  <Link to="/explore" className="button btn-solid button-secondary">
+                    Go to Explore
+                  </Link>
+                </div>
+              </>
+            )}
+          </div>
+        </section>
+      </main>
+    </section>
+  );
+};
+
+export default WatchLaterPage;
