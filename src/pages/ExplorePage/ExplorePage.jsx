0cda0ff126e6ffd5fd802c05aa6f9ff882ee70c5
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2022-06-11 20:54:13 +0530

    feat: added filter by category

--- src/pages/ExplorePage/ExplorePage.jsx
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { NavigationSide } from "components";
-import { VideoCard } from "components";
+import { VideoCard, CategoryFilter } from "components";
 import { useVideos } from "contexts";
 import "./explore-page.css";
 
 const ExplorePage = () => {
-  const { videosLoading, videos } = useVideos();
+  const { videosLoading, videos } =
+    useVideos();
   const [loader, setLoader] = useState(true);
 
   useEffect(() => {
@@ -35,6 +36,7 @@ const ExplorePage = () => {
       ) : (
         <>
           <main className="video-grid-item" id="video-main">
+            <CategoryFilter />
             <section className="video-container">
               {videos.map((video) => {
                 return <VideoCard video={video} key={video._id} />;
